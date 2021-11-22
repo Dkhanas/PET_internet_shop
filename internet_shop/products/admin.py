3e92496afad73fda40de11e5512cea5f2c97998e
StepanTchynetskyi <tchynetsky@gmail.com>
2021-11-22 20:00:49 +0200

    added django fixture, fixed models, added constants

--- internet_shop/products/admin.py
+++ internet_shop/products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Currency, Characteristic, Image, Catalog, Product, ProductImage, ProductCharacteristic
 # Register your models here.
+admin.site.register(Currency)
+admin.site.register(Characteristic)
+admin.site.register(Image)
+admin.site.register(Catalog)
+admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(ProductCharacteristic)
--- internet_shop/products/models.py
+++ internet_shop/products/models.py
@@ -1,47 +1,62 @@
 from django.db import models
-import uuid
+from utils.constants import NAME_CHARFIELD_LENGTH, CODE_CHARFIELD_LENGTH, HUGE_CHARFIELD_LENGTH, \
+    PRICE_DECIMALFIELD_DIGITS_LENGTH, PRICE_DECIMALFIELD_DECIMAL_LENGTH
 from mptt.models import MPTTModel, TreeForeignKey
 
+from utils.base_models import BaseModel
 
-class BaseModel(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+
+class Currency(BaseModel):
+    name = models.CharField(max_length=NAME_CHARFIELD_LENGTH, unique=True)
+    exchange_rate = models.DecimalField(max_digits=PRICE_DECIMALFIELD_DIGITS_LENGTH,
+                                        decimal_places=PRICE_DECIMALFIELD_DECIMAL_LENGTH)
 
     class Meta:
-        abstract = True
+        verbose_name_plural = "Currencies"
 
-    def __str__(self):
-        if hasattr(self, 'name'):
-            return self.name
+
+class Characteristic(BaseModel):
+    name = models.CharField(max_length=NAME_CHARFIELD_LENGTH, unique=True)
+    value = models.CharField(max_length=HUGE_CHARFIELD_LENGTH)
+
+
+class Image(BaseModel):
+    image_url = models.TextField(blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        # TODO: add s3 bucket support
+        super().save(force_insert, force_update, using, update_fields)
 
 
 class Catalog(MPTTModel, BaseModel):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=NAME_CHARFIELD_LENGTH)
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    image = models.ForeignKey(Image, related_name='catalog', on_delete=models.SET_NULL, null=True, blank=True)
 
     class MPTTMeta:
         order_insertion_by = ['name']
 
 
-class Currency(BaseModel):
-    name = models.CharField(max_length=50, unique=True)
-    exchange_rate = models.DecimalField(max_digits=9, decimal_places=2)
-
-
-class Characteristic(BaseModel):
-    name = models.CharField(max_length=70, unique=True)
-    value = models.CharField(max_length=150)
-
-
 class Product(BaseModel):
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=9, decimal_places=2)
-    image = models.TextField()
+    name = models.CharField(max_length=NAME_CHARFIELD_LENGTH)
+    code = models.CharField(max_length=CODE_CHARFIELD_LENGTH)
+    description = models.CharField(max_length=HUGE_CHARFIELD_LENGTH)
+    price = models.DecimalField(max_digits=PRICE_DECIMALFIELD_DIGITS_LENGTH,
+                                decimal_places=PRICE_DECIMALFIELD_DECIMAL_LENGTH)
+    old_price = models.DecimalField(max_digits=PRICE_DECIMALFIELD_DIGITS_LENGTH,
+                                    decimal_places=PRICE_DECIMALFIELD_DECIMAL_LENGTH)
     quantity = models.PositiveSmallIntegerField()
+    category = models.ForeignKey(Catalog, related_name='products', on_delete=models.SET_NULL, null=True)
 
 
-class Product_Characteristic(BaseModel):
-    product_id = models.ForeignKey(Product, related_name='product_characteristics', on_delete=models.CASCADE)
-    characteristic_id = models.ForeignKey(Characteristic, related_name='product_characteristics', on_delete=models.CASCADE)
+class ProductImage(BaseModel):
+    image = models.ForeignKey(Image, related_name='product_images', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='product_images', on_delete=models.CASCADE)
+
+
+class ProductCharacteristic(BaseModel):
+    product = models.ForeignKey(Product, related_name='product_characteristics', on_delete=models.CASCADE)
+    characteristic = models.ForeignKey(Characteristic, related_name='product_characteristics',
+                                       on_delete=models.CASCADE)
