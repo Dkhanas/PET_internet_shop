0eb25f9c377c435589368cca5f366ef3948d221f
BodiaPavlyk <bogdan2002mih@gmail.com>
2021-12-14 17:01:26 +0200

    Created migration for User, added AUTHENTICATION_BACKENDS to settings file

--- internet_shop/internet_shop/settings.py
+++ internet_shop/internet_shop/settings.py
@@ -89,6 +89,10 @@ DATABASES = {
     }
 }
 
+AUTHENTICATION_BACKENDS = [
+    'user.authentication.UserAuthBackend',
+    'django.contrib.auth.backends.ModelBackend'
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
